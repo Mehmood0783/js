3f1ee54a3b4e716d7be6fbdad6ad5439015c7340
Mehmood Akmal <142147892+Mehmood0783@users.noreply.github.com>
2023-10-26 09:30:40 +0000

    variables done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 124575
+let accountEmail = "Akmal@gmail.com"
+var accountPassword = "123456"
+accountCity = "sargodha"
+
+// accountId = 5 // not Allowed
+
+accountEmail = "ab@ab.com"
+accountPassword = "121212"
+accountCity = "New city"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
