008331a508669ce1179f61a8c147c996862e5397
Mehmood Akmal <142147892+Mehmood0783@users.noreply.github.com>
2023-10-26 09:51:00 +0000

    datatypes done

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using node.js not browser
+
+ let name = "Akmal"
+ let age = 25
+ let isLogedIn = false
+
+//  number => 2 to power 53
+// string => ""
+// bigInt
+// boolean => true/false
+// null => standalon value
+// unDefined
+// symbol => unique
+
+// object
+
+
+// typeOf
+console.log(typeof null); //output is object
+console.log(typeof undefined); // undefined
+
+
