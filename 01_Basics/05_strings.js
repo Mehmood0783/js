4123651845d77891e24385e35cb1f0157f593be0
Mehmood Akmal <142147892+Mehmood0783@users.noreply.github.com>
2023-10-29 07:58:42 +0000

    strings methods prototype

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,13 @@
+const name = "Akmal";
+const repoCount = 50;
+
+// console.log("my name is ${name} and my total repository on github is ${repoCount}");
+
+
+// how to declair a string
+const gameName = new String('akmal') // now this string is an object 
+console.log(gameName.length);
+console.log(gameName.__proto__);
+console.log(gameName.toUpperCase());
+// console.log(gameName.repeat(5));
+console.log(gameName.indexOf(5));
